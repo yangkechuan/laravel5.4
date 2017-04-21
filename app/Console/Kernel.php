4b835b209fbe6edf78adf8d97ba3c100c7a173db
yangkechuan <907148778@qq.com>
2017-04-21 11:24:18 +0800

    修改定时任务

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,13 +29,11 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
 
-        $schedule->exec('sh /data/ngx_openresty/nginx/html/ApiTest/start.sh')->hourly()->withoutOverlapping();
-        $schedule->exec('cd /data/ngx_openresty/nginx/html/laravel && envoy run web')->everyMinute()->withoutOverlapping();
-        $schedule->exec('cd /data/ngx_openresty/nginx/html/laravel && envoy run api')->everyMinute()->withoutOverlapping();
+        $schedule->exec('sh /data/ngx_openresty/nginx/html/ApiTest/start.sh')->hourly();
 
         $schedule->call(function (){
             ApiLog::where('created_at', '<', Carbon::now()->addDays(-3) )->delete();
-        })->daily()->withoutOverlapping();
+        })->daily();
     }
 
     /**
